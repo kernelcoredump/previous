268610d1d5cbb1d738e09237dde1db183012b138
kernelcoredump <1821465+kernelcoredump@users.noreply.github.com>
2020-10-27 20:23:29 -0700

    use cd instead of pushd

--- .github/actions/linuxbuilder/build.sh
+++ .github/actions/linuxbuilder/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir build && pushd build
+mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
 cmake --build . --config Release
 make install DESTDIR=dist
